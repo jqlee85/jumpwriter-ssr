2b73884f5e1697688108537509fea99f7cf5a937
Jesse Lee <jqlee85@gmail.com>
2017-11-06 23:20:22 -0600

    before sir

--- src/server/app.js
+++ src/server/app.js
@@ -2,11 +2,15 @@ var express = require('express');
 var app = express();
 var favicon = require('serve-favicon');
 
+app.use(express.static(__dirname +'./../../')); //serves the index.html
+
 // Favicon
 app.use(favicon(__dirname +'./../client/public/favicon.ico'));
 
-app.use(express.static(__dirname +'./../../')); //serves the index.html
-app.listen(3001); //listens on port 3001 -> http://localhost:3001/
+// Set Port
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT); //listens on port 3001 -> http://localhost:3001/
 
 // Console Notification
 var date = new Date();
